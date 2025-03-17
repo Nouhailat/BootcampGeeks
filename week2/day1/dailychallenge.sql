c40a4ddfe3c428fd3c932a5ca38cc6724cb0a8ff
nouha touati <nouhaila.ribery@gmail.com>
2025-03-17 18:12:05 +0000

    daily challenge

---
+++ week2/day1/dailychallenge.sql
@@ -0,0 +1,14 @@
+-- CREATE TABLE actors (
+--     id SERIAL PRIMARY KEY,
+--     first_name VARCHAR(50) NOT NULL,
+--     last_name VARCHAR(50) NOT NULL,
+--     birth_date DATE
+-- );
+-- SELECT COUNT(*) FROM actors;
+-- INSERT INTO actors (first_name, last_name, birth_date) 
+-- VALUES ('', '', NULL);
+-- SELECT column_name, is_nullable 
+-- FROM information_schema.columns 
+-- WHERE table_name = 'actors';
+
+
