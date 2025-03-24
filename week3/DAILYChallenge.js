93ac06767f1c71cd7e398407ea62db022c6ec40b
nouha touati <nouhaila.ribery@gmail.com>
2025-03-24 12:15:56 +0000

    DAILY CHALLENGE

---
+++ week3/DAILYChallenge.js
@@ -0,0 +1,12 @@
+
+  let sentence="This dinner is not that bad ! You cook well"
+  let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
+if (wordNot !== -1 && wordBad !== -1 && wordBad > wordNot) {
+   
+    let updatedSentence = sentence.substring(0, wordNot) + "good" + sentence.substring(wordBad + 3);
+    console.log(updatedSentence);
+} else {
+    
+    console.log(sentence);
+}
