2fb1f71dda1573cbf72995928414bf1bd9d89415
nouha touati <nouhaila.ribery@gmail.com>
2025-03-24 12:38:15 +0000

    DAILY CHALLENge happy birthday

---
+++ week1/day1/DAILYCHALLENGE.py
@@ -0,0 +1,10 @@
+
+birthdate = input("Enter your birthdate (12/06/2002): ")
+
+print("       ___iiiii___")
+print("      |:H:a:p:p:y:|")
+print("    __|___________|__")
+print("   |^^^^^^^^^^^^^^^^^|")
+print("   |:B:i:r:t:h:d:a:y:|")
+print("   |                 |")
+print("   ~~~~~~~~~~~~~~~~~~~")
