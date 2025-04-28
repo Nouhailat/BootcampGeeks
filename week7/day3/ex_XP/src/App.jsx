0aac1e4dfd1fe535c7268239a26a81205fc64418
Nouhailat <nouhaila.ribery@gmail.com>
2025-04-28 12:34:34 +0100

    ex1

---
+++ week7/day3/ex_XP/src/App.jsx
@@ -0,0 +1,23 @@
+import BuggyCounter from './Components/BuggyCounter'
+import ErrorBoundary from './Components/ErrorBoundary'
+
+function App() {
+  return (
+    <div>
+      <h1>Simulation 1</h1>
+      <ErrorBoundary>
+        <BuggyCounter />
+        <BuggyCounter />
+      </ErrorBoundary>
+
+      <h1>Simulation 2</h1>
+      <ErrorBoundary><BuggyCounter /></ErrorBoundary>
+      <ErrorBoundary><BuggyCounter /></ErrorBoundary>
+
+      <h1>Simulation 3</h1>
+      <BuggyCounter />
+    </div>
+  )
+}
+
+export default App
